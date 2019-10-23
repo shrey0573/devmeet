717484562ae44a88d12f0e589451eaefc7dfbbc4
Shrey Chauhan <shreychauhan0573@gmail.com>
2019-10-24 00:54:59 +0530

    Initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <h1> Dev-Meet </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
