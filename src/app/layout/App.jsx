65635b6e229e76302010d50c64a3e625c0134e96
Shrey Chauhan <shreychauhan0573@gmail.com>
2019-10-25 09:42:23 +0530

    Commit

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
 import React,{Component} from 'react';
+import { Button } from 'semantic-ui-css';
 
 class App extends Component{
   render(){
     return (
       <div>
         <h1> Dev-Meet </h1>
+        <button className="ui icon">
+          <i icon="smile icon"></i>
+          CSS Button
+        </button>
+        <Button icon="smile" content= "React Button" />
       </div>
     );
   }
